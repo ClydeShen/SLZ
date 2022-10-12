a0d497e13e978cb3440c53a079cef2066f2b814e
Clyde Shen <clyde.shen@outlook.com>
2022-10-12 17:30:59 +1300

    init next

--- pages/_app.js
+++ pages/_app.js
@@ -1,46 +1,7 @@
-import Head from "next/head";
-import Router from "next/router";
-import nProgress from 'nprogress';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import { CacheProvider } from "@emotion/react";
-import { createTheme } from '../theme';
-import { createEmotionCache } from '../utils/create-emotion-cache';
+import '../styles/globals.css'
 
-Router.events.on("routeChangeStart", nProgress.start);
-Router.events.on("routeChangeError", nProgress.done);
-Router.events.on("routeChangeComplete", nProgress.done);
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
 
-const clientSideEmotionCache = createEmotionCache();
-const initialSettings = {
-  direction: 'ltr',
-  responsiveFontSizes: true,
-  theme: 'dark'
-};
-const App = (props) => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
-  const getLayout = Component.getLayout ?? ((page) => page);
-
-  return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>SLZ</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-
-      <ThemeProvider
-        theme={createTheme({
-          direction: initialSettings.direction,
-          responsiveFontSizes: initialSettings.responsiveFontSizes,
-          mode: initialSettings.theme,
-        })}
-      >
-        <CssBaseline />
-        {getLayout(<Component {...pageProps} />)}
-      </ThemeProvider>
-    </CacheProvider>
-  );
-};
-App.displayName="App"
-export default App;
+export default MyApp
