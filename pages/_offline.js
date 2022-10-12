2beea6d491fa8c6d8a28ab388b359f1d9b741c1a
Clyde Shen <clyde.shen@outlook.com>
2022-10-12 21:26:18 +1300

    folder structure

---
+++ pages/_offline.js
@@ -0,0 +1,8 @@
+import React, { memo } from 'react'
+
+const OfflinePage = memo(() => {
+  return <div>You are offline</div>
+})
+
+OfflinePage.displayName = 'OfflinePage'
+export default OfflinePage
